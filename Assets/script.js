cc2e6c0ab51753eff170c816c7b7e0706fc2cc94
Raimeeab <raimee.abbassi@gmail.com>
2021-09-05 09:47:30 +1000

    basic logic operating

--- Assets/script.js
+++ Assets/script.js
@@ -1,57 +1,72 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-//user input variables
-var passwordLength, confirmUpper, confirmLower, confirmNumber, confirmSymbol; 
-
 // Password variable values 
 var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var symbols = ["!","@","#","$","%","^","&","*","(",")","{","}","[","]","","<",">","/",",","."]
-var numbers = ["0","1","2","3","4","5","6","7","8","9"];
+var symbol = ["!","@","#","$","%","^","&","*","(",")","{","}","[","]","","<",">","/",",","."]
+var number = ["0","1","2","3","4","5","6","7","8","9"];
+console.log (symbol.length);
 
+var allCharacters = [...lowercase, ...uppercase, ...symbol, ...number]; //creates an object that includes all the arrays above 
+var confirmed = false; // when user doesn't meet two conditions 
+
+var password = [];
+
+
+var generateBtn = document.querySelector("#generate");
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", userInput);
 
-// Create password based on user selections 
+//user input variables
+var passwordLength, confirmUpper, confirmLower, confirmNumeric, confirmSymbol; 
 
 
-// Write password to input
-function writePassword() {
-  // var password = generatePassword();
-  // var passwordText = document.querySelector("#password");
+//Generate Password function, selecting random chracters based on what is confirmed by the user 
+function generatePassword() {
+  confirmLower = confirm('would you like to include lowercase letters?');
+  confirmUpper = confirm('would you like to include uppercase letters?');
+  confirmNumeric = confirm('would you like to include numbers?');
+  confirmSpecial = confirm('would you like to include special characters?');
+ 
+  if (!confirmLower && !confirmUpper && !confirmNumeric && !confirmSpecial) {
+    choices = alert("Error: you must choose atleast one of the character options");
+  };
 
-  // passwordText.value = password;
-  enter = parseInt(prompt("How many characters would you like your password?"));
-  if (!enter) {
-    alert("Value is required");
-  } else if (enter < 8 || enter > 128) {
-    enter = parseInt(alert("You must choose between 8 and 128 characters"));
-   // Ensures user input for password length is between 8 and 128 characters 
+  for (var i = 0; i < passwordLength; i++) {
+    if (confirmLower) {
+      password.push(lowercase[Math.floor(Math.random() * 26)]);
+    } 
+    if (confirmUpper) {
+      password.push(uppercase[Math.floor(Math.random() * 26)]);
+    }
+    if (confirmNumeric) {
+      password.push(number[Math.floor(Math.random() * 10)]);
+    if (confirmSymbol) {
+      password.push(symbol[Math.floor(Math.random() * 20)]);
+    }
+    };
+  };
+  console.log(password);
+  return password.join("");
+};
+
+// controls prompts/ alerts for user input 
+function userInput() {
+  passwordLength = parseInt(prompt("How many characters would you like your password to be?"));
+  if (!passwordLength) {
+    alert("Error: value is required");
+
+  } else if (passwordLength <= 8 || passwordLength >= 128) {
+    passwordLength = parseInt(prompt("Error: you must choose between 8 and 128 characters"));
 
   } else {
-    confirmLower = confirm("Do you want lowercase letters?");
-    confirmUpper = confirm("Do you want uppercase letters?");
-    confirmSymbol = confirm("Do you want symbols?");
-    confirmNumber = confirm("Do you want numbers?");
-  }
-  
-  if (!confirmLower && !confirmUpper && !confirmSymbol && !confirmNumber) {
-    userChoice = alert("You must select atleast one of the character options");
-    // Ensures that user is required to select atleast one of the options provided 
-  }
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+
+    passwordText.value = password;
+  };
 
 };
 
-
-// function generatePassword() {
-//   var userInfo = userChoices();
-
-// }
-
-// Generator functions 
-
-// function getRandomLower() {
-//   return string.fromCharCode(Math.floor(Math.random() * 26) + 97)
+// while(passwordLength >= 8 || passwordLength <= 128) {
+//   alert ("Error: you much choose between 8 and 128 characters");
+//   return alert ("Press Generate button to try again");
 // };
-
